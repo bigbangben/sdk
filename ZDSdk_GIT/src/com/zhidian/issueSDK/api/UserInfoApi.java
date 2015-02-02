a4d3877ac524cf980a3c3a67a6d92835ec470c89
bigbun <1124371650@qq.com>
2015-02-02 17:04:04 +0800

    aaaa

---
+++ ZDSdk_GIT/src/com/zhidian/issueSDK/api/UserInfoApi.java
@@ -0,0 +1,39 @@
+package com.zhidian.issueSDK.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.zhidian.issueSDK.net.HttpEngine;
+import com.zhidian.issueSDK.net.StringRequest;
+import com.zhidian.issueSDK.service.InitService;
+import com.zhidian.issueSDK.util.SDKUtils;
+
+/**
+ * @Description
+ * @author ZengQBo
+ * @time 2015年1月30日
+ */
+public class UserInfoApi extends StringRequest {
+
+	public String sid;
+	public String gameId;
+	
+	
+	@Override
+	public String getUrl() {
+		return Url.BASE_URL + Url.GET_USERINFO_URL;
+	}
+
+	@Override
+	public HttpEngine.Method getMethod() {
+		return HttpEngine.Method.POST;
+	}
+	
+	@Override
+	public Map<String, String> getParams() {
+		Map paramMap = new HashMap();
+		paramMap.put("sid", sid);
+		paramMap.put("gameId", gameId);
+		return paramMap;
+	}
+}
