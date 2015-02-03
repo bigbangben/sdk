f68619e04dcf219b2f68936a60d3d4555aae4f5f
bigbun <1124371650@qq.com>
2015-02-03 14:37:42 +0800

    aaa

--- ZDSdk_GIT/src/com/zhidian/issueSDK/platform/OppoPlatform.java
+++ ZDSdk_GIT/src/com/zhidian/issueSDK/platform/OppoPlatform.java
@@ -2,8 +2,12 @@ package com.zhidian.issueSDK.platform;
 
 import android.app.Activity;
 
+import com.nearme.gamecenter.open.api.ApiCallback;
 import com.nearme.gamecenter.open.api.GameCenterSDK;
+import com.nearme.gamecenter.open.api.GameCenterSettings;
+import com.nearme.oauth.model.UserInfo;
 import com.zhidian.issueSDK.model.GameInfo;
+import com.zhidian.issueSDK.model.UserInfoModel;
 import com.zhidian.issueSDK.service.CreateRoleService.CreateRoleListener;
 import com.zhidian.issueSDK.service.ExitService.GameExitListener;
 import com.zhidian.issueSDK.service.InitService.GameInitListener;
@@ -11,9 +15,12 @@ import com.zhidian.issueSDK.service.LogOutService.GameLogoutListener;
 import com.zhidian.issueSDK.service.LoginService.GameLoginListener;
 import com.zhidian.issueSDK.service.OrderGenerateService.OrderGenerateListener;
 import com.zhidian.issueSDK.service.SetGameInfoService.SetGameInfoListener;
+import com.zhidian.issueSDK.util.SDKLog;
 import com.zhidian.issueSDK.util.SDKUtils;
 
 public class OppoPlatform implements Iplatform {
+
+	private static final String TAG = "OppoPlatform";
 
 	public OppoPlatform() {
 	}
@@ -24,13 +31,75 @@ public class OppoPlatform implements Iplatform {
 	}
 
 	@Override
-	public void init(Activity mActivity, GameInitListener gameInitListener,
+	public void init(Activity activity, GameInitListener gameInitListener,
 			GameLoginListener gameLoginListener) {
+		SDKLog.e(TAG, "begin init");
+		GameCenterSDK.setmCurrentContext(activity);
+		String screenOrientation = SDKUtils.getMeteData(activity,
+				"screenOrientation");
+		GameCenterSettings.isOritationPort = screenOrientation.equals("0") ? false
+				: true;
+		GameCenterSettings.isDebugModel = true;
+		GameCenterSettings.proInnerSwitcher = false;
+		// 测试用的appkey和secret
+		// TODO 这个里的为测试key和secret，请务必替换为正式的！
+		GameCenterSettings gameCenterSettings = new GameCenterSettings(
+				"c5217trjnrmU6gO5jG8VvUFU0", "e2eCa732422245E8891F6555e999878B") {
+
+			@Override
+			public void onForceReLogin() {
+				// sdk由于某些原因登出,此方法通知cp,cp需要在此处清理当前的登录状态并重新请求登录.
+				// 可以发广播通知页面重新登录
+			}
+
+			@Override
+			public void onForceUpgradeCancel() {
+				// 游戏自升级，后台有设置为强制升级，用户点击取消时的回调函数。
+				// 若开启强制升级模式 ， 一般要求不更新则强制退出游戏并杀掉进程。
+				// System.exit(0) or kill this process
+			}
+		};
+		// TODO for test old
+		// AccountAgent.useNewApi = true;
+		GameCenterSDK.init(gameCenterSettings, activity);
+
 	}
 
 	@Override
-	public void login(Activity activity, GameLoginListener gameLoginListener) {
-		// TODO Auto-generated method stub
+	public void login(final Activity activity, final GameLoginListener gameLoginListener) {
+		GameCenterSDK.setmCurrentContext(activity);
+		GameCenterSDK.getInstance().doLogin(new ApiCallback() {
+
+			@Override
+			public void onSuccess(String content, int code) {
+
+				GameCenterSDK.getInstance().doGetUserInfo(new ApiCallback() {
+
+					@Override
+					public void onSuccess(String content, int code) {
+						try {
+							UserInfo userInfo = new UserInfo(content);
+							UserInfoModel model = new UserInfoModel();
+							model.id = userInfo.id;
+							model.userName = userInfo.username;
+							gameLoginListener.LoginSuccess(model);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+
+					@Override
+					public void onFailure(String content, int code) {
+						gameLoginListener.LoginFail(content);
+					}
+				}, activity);
+				
+			}
+
+			@Override
+			public void onFailure(String content, int code) {
+			}
+		}, activity);
 
 	}
 
