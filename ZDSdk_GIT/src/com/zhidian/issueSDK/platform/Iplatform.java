142d8626a8871f73cd73e452039f0bb4f35aded1
bigbun <1124371650@qq.com>
2015-03-09 11:38:01 +0800

    ffdsfew

--- ZDSdk_GIT/src/com/zhidian/issueSDK/platform/Iplatform.java
+++ ZDSdk_GIT/src/com/zhidian/issueSDK/platform/Iplatform.java
@@ -83,6 +83,8 @@ public interface Iplatform {
 	public boolean suportLogoutUI();
 
 	public void onPause(Activity activity);
+	
+	public void onStop(Activity activity);
 
 	public void onResume(Activity activity);
 
